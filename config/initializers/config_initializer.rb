e3a2e55477b495541c7a0afc3a8576eb1004c993
noxowl <nachtbeere@outlook.com>
2020-04-28 03:00:06 +0900

    remove config initializer

--- config/initializers/config_initializer.rb
+++
@@ -1,5 +0,0 @@
-require 'ostruct'
-require 'yaml'
-
-_config = YAML.load_file(Rails.root.join("config/config.yml"))[Rails.env] || YAML.load_file(Rails.root.join("config/config.yml.example"))[Rails.env]
-ADDITIONAL_CONFIG = OpenStruct.new(_config)
