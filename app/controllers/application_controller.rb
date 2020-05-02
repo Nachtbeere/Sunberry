490e16542cd12f1b8e5210d2a0c82fd50745ac1e
noxowl <nachtbeere@outlook.com>
2020-05-02 17:18:28 +0900

    add set_protocol https in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   helper_method :logged_in?
+  before_action :set_protocol if Rails.env.production?
+
+  def set_protocol
+    Rails.application.routes.default_url_options[:protocol] = "https"
+  end
 
   def current_user
     User.find_by(id: session[:user_id])
