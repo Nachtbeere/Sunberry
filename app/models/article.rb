d520fb98c0339ffa150341523763907d49ff6e1e
noxowl <nachtbeere@outlook.com>
2020-05-08 18:44:48 +0900

    add same_author? method

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,14 @@ class Article < ApplicationRecord
   has_rich_text :content
   acts_as_punchable
 
+  def same_category?(category)
+    self.category == category
+  end
+
+  def same_author?(author_id)
+    user_id == author_id
+  end
+
   def self.get_by_page(page, category)
     Article.page(page).per(10).order(id: :desc).where category: ADDITIONAL_CONFIG["article_categories"].find_index(category)
   end
